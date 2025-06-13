bc2726d0b0cd32895f9df06487a04e2ab75e5fc8
LouisLP <louis.lascelles.palys@gmail.com>
2025-06-13 10:11:48 +0200

    Navbar with reference link to avoid going back-and-forth

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -4,6 +4,29 @@ import RichTextEditor from '../components/RichTextEditor.vue'
 
 <template>
   <main id="homepage">
+    <!-- Navbar -->
+    <nav class="flex justify-between items-center px-6 py-4 bg-slate-50 shadow-md mb-8">
+      <!-- Logo -->
+      <div class="flex items-center gap-2 font-bold tracking-wide text-xl">
+        <span>Page Breaker</span>
+        <img
+          src="/public/favicon_io/apple-touch-icon.png"
+          alt="Broken heart logo"
+          class="size-4 animate-pulse mt-1"
+        />
+      </div>
+      <!-- Right-side link -->
+      <a
+        href="https://app.avayl.tech/coding_task.html"
+        target="_blank"
+        rel="noopener noreferrer"
+        class="text-base font-medium text-blue-600 hover:underline"
+      >
+        Reference
+      </a>
+    </nav>
+
+    <!-- Editor -->
     <RichTextEditor />
   </main>
 </template>
